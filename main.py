eae03b77668c0ad99d5e370845d11cb2bf84f2f2
Ibikunleogunbanwo <ibikunleogunbanwo@gmail.com>
2025-07-02 20:34:53 -0600

    updates

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List
 
+
+# Inherit the Basemodel class from pydantic
 class Stadium(BaseModel):
     id: int | None = None
     name: str
@@ -11,14 +13,20 @@ class Stadium(BaseModel):
     club: str | None = None
     open_year: int
 
-app = FastAPI(title="Stadium Directory")
+
+
+app = FastAPI(title="Stadium Endpoints v1")
+
+
 
 DB_PATH = "stadiums.db"
 
+
+
 @app.on_event("startup")
 def init_db():
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
+    db_conn = sqlite3.connect(DB_PATH)
+    cursor = db_conn.cursor()
     cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS stadia(
@@ -32,21 +40,25 @@ def init_db():
 
         """
     )
-    conn.commit()
-    conn.close()
+    db_conn.commit()
+    db_conn.close()
+
+
+
 
 def get_db():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
+    db_conn  = sqlite3.connect(DB_PATH)
+    db_conn.row_factory = sqlite3.Row
     try:
-        yield conn
+        yield db_conn 
     finally:
-        conn.close()
+        db_conn.close()
+
 
 
 @app.post("/create_stadium/", response_model=Stadium, status_code=201)
-def add_stadium(stadium: Stadium, db: sqlite3.Connection = Depends(get_db)):
-    cursor = db.cursor()
+def add_stadium(stadium: Stadium, db_conn : sqlite3.Connection = Depends(get_db)):
+    cursor = db_conn.cursor()
     try:
         cursor.execute(
             """
@@ -55,7 +67,7 @@ def add_stadium(stadium: Stadium, db: sqlite3.Connection = Depends(get_db)):
             """,
             (stadium.name, stadium.city, stadium.capacity,stadium.club,stadium.open_year)
         )
-        db.commit()
+        db_conn.commit()
     except sqlite3.IntegrityError as e:
         raise HTTPException(400, detail="Stadium with thesame name alrady exists")
     stadium.id = cursor.lastrowid
@@ -63,17 +75,19 @@ def add_stadium(stadium: Stadium, db: sqlite3.Connection = Depends(get_db)):
 
 
 
+
 @app.get('/stadiums/', response_model=List[Stadium])
-def list_stadiums(db: sqlite3.Connection = Depends(get_db)):
-    cursor = db.execute("SELECT * FROM stadia")
-    rows = cursor.fetchall()
-    return [Stadium(**row) for row in rows]
+def list_stadiums(db_conn : sqlite3.Connection = Depends(get_db)):
+    cursor = db_conn.execute("SELECT * FROM stadia")
+    all_stadia = cursor.fetchall()
+    return [Stadium(**each_stadium) for each_stadium in all_stadia]
+
 
 
 
 @app.get("/stadium/{stadium_id}", response_model=Stadium)
-def get_stadium_by_id(stadium_id: int,db: sqlite3.Connection = Depends(get_db)):
-    cursor = db.execute("SELECT * FROM stadia WHERE id = ?", (stadium_id,))
+def get_stadium_by_id(stadium_id: int, db_conn: sqlite3.Connection = Depends(get_db)):
+    cursor = db_conn.execute("SELECT * FROM stadia WHERE id = ?", (stadium_id,))
     row = cursor.fetchone()
     if not row:
         raise HTTPException(404, detail="Stadium not found")
@@ -82,8 +96,8 @@ def get_stadium_by_id(stadium_id: int,db: sqlite3.Connection = Depends(get_db)):
 
 
 @app.get("/stadium_name/{stadium_name}", response_model=Stadium)
-def get_stadium_by_name(stadium_name: str,db: sqlite3.Connection = Depends(get_db)):
-    cursor = db.execute("SELECT * FROM stadia WHERE lower(name) = ?", (stadium_name.lower(),))
+def get_stadium_by_name(stadium_name: str, db_conn: sqlite3.Connection = Depends(get_db)):
+    cursor = db_conn.execute("SELECT * FROM stadia WHERE lower(name) = ?", (stadium_name.lower(),))
     row = cursor.fetchone()
     if not row:
         raise HTTPException(404, detail="Stadium not found")
@@ -91,9 +105,10 @@ def get_stadium_by_name(stadium_name: str,db: sqlite3.Connection = Depends(get_d
 
 
 
+
 @app.put("/stadium_by_id/{stadium_id}", response_model=Stadium)
-def update_stadium(stadium_id: int, data: Stadium, db: sqlite3.Connection = Depends(get_db)):
-    cursor = db.cursor()
+def update_stadium(stadium_id: int, data: Stadium, db_conn: sqlite3.Connection = Depends(get_db)):
+    cursor = db_conn.cursor()
     cursor.execute ("select * from stadia where id = ?", (stadium_id,))
     if not cursor.fetchone():
         raise HTTPException(status_code=404, detail="stadium not found")
@@ -106,19 +121,22 @@ def update_stadium(stadium_id: int, data: Stadium, db: sqlite3.Connection = Depe
                     (data.name, data.city, data.club, data.capacity, data.open_year, data.id)
 
                     )
-    db.commit()
+    db_conn.commit()
     data.id = stadium_id
     return data
 
+
+
+
 @app.delete("/stadium/{stadium_id}", status_code=204)
-def delete_stadium (stadium_id: int, db: sqlite3.Connection = Depends(get_db)):
-    cursor = db.cursor()
+def delete_stadium (stadium_id: int, db_conn: sqlite3.Connection = Depends(get_db)):
+    cursor = db_conn.cursor()
     cursor.execute("select id from stadia where id =?", (stadium_id,))
     if not cursor.fetchone():
         return HTTPException(status_code=400, detail="stadium not found")
     
     cursor.execute("delete from stadia where id = ?", (stadium_id,))
-    db.commit()
+    db_conn.commit()
     return
 
     
